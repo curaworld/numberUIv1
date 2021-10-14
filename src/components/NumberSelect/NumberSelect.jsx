7f22f0db3cfb755469a37138f243a25a56850b90
Clinton Baird <43327946+curaworld@users.noreply.github.com>
2021-10-14 01:10:55 -0500

    added numbers

--- src/components/NumberSelect/NumberSelect.jsx
+++ src/components/NumberSelect/NumberSelect.jsx
@@ -5,9 +5,29 @@ import MenuItem from "@material-ui/core/MenuItem";
 import * as Flex from "@twilio/flex-ui";
 
 // It is recommended to keep components stateless and use redux for managing states
-const NumberSelect = (props) => {
-  let callerIds = Flex.Manager.getInstance().workerClient.attributes.callerIds;
 
+// names = {
+  
+//   '+17312022600': 'The Kidney Experts',
+//   '+17207039680': 'Western Nephrology',
+//   '+14698091098': 'Dallas Renal Group',
+//   '+14052884181': 'Dallas Renal Lawton',
+//   '+19182198072': 'Tulsa CardioVascular',
+//   '+15802034030': 'Oklahoma Heart Hospital',
+//   '+18172415127': 'Dr. Lee',
+//   '+16207401025': 'Allied Natoinal',
+//   '+18555182872': 'Kings Hawaiian',
+//   '+18889102872': 'Cura Main',
+//   '+15124099903': 'ONI Austin',
+//   '+14054071025': 'ONI Meeker',
+//   '+19184011002': 'ONI Tulsa' 
+// }
+
+const NumberSelect = (props) => {
+  console.log('FlexgetInstance',Flex.Manager.getInstance())
+  let callerIds = Flex.Manager.getInstance().workerClient.attributes.callerIds;
+  console.log(callerIds, 'CALLERIDS')
+  
   return (
     <div>
       <Caption
@@ -25,7 +45,7 @@ const NumberSelect = (props) => {
         </MenuItem>
         {callerIds.map((element) => (
           <MenuItem key={element} value={element}>
-            {element}
+           {element}  
           </MenuItem>
         ))}
       </StyledSelect>
